719869e44c2063699e5bac69a144c4f3ccc4e484
Chris Koch <chrisko@google.com>
2019-09-01 17:57:36 -0700

    filetest: add p9.File acceptance test framework and first test
    Signed-off-by: Chris Koch <chrisko@google.com>

--- localfs/localfs.go
+++ localfs/localfs.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package localfs exposes the host's local file system as a p9.File.
 package localfs
 
 import (
