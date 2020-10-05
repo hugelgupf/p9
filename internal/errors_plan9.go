b51fd9aab7adcef8e14e9b224b0c4507ad4da923
gopher9 <72321653+gopher9@users.noreply.github.com>
2020-10-04 22:56:18 -0500

    basic error handling

--- internal/errors_plan9.go
+++ internal/errors_plan9.go
@@ -3,23 +3,19 @@
 package internal
 
 import (
-	//"syscall"
+	"syscall"
 
 	"github.com/hugelgupf/p9/internal/linux"
 )
 
 func sysErrno(err error) linux.Errno {
 	switch err {
-	//case syscall.ERROR_FILE_NOT_FOUND:
-	//	return linux.ENOENT
-	//case syscall.ERROR_PATH_NOT_FOUND:
-	//	return linux.ENOENT
-	//case syscall.ERROR_ACCESS_DENIED:
-	//	return linux.EACCES
-	//case syscall.ERROR_FILE_EXISTS:
-	//	return linux.EEXIST
-	//case syscall.ERROR_INSUFFICIENT_BUFFER:
-	//	return linux.ENOMEM
+	case syscall.ENOENT:
+		return linux.ENOENT
+	case syscall.EACCES:
+		return linux.EACCES
+	case syscall.EEXIST:
+		return linux.EEXIST
 	default:
 		// No clue what to do with others.
 		return 0
