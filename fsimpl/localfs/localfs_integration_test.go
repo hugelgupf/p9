3b0f26fc625879b7de72214c104376a274becfca
Chris Koch <chrisko@google.com>
2022-12-17 23:39:19 -0800

    Read & write benchmarks using DD
    Signed-off-by: Chris Koch <chrisko@google.com>

--- fsimpl/localfs/localfs_integration_test.go
+++ fsimpl/localfs/localfs_integration_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hugelgupf/p9/fsimpl/test/vmdriver"
 	"github.com/hugelgupf/p9/p9"
 	"github.com/u-root/u-root/pkg/qemu"
+	"github.com/u-root/u-root/pkg/uroot"
 	"github.com/u-root/u-root/pkg/vmtest"
 	"github.com/u-root/uio/ulog/ulogtest"
 )
@@ -38,6 +39,48 @@ func TestIntegration(t *testing.T) {
 
 	// Run the read-write tests from fsimpl/test/rwvm.
 	vmtest.GolangTest(t, []string{"github.com/hugelgupf/p9/fsimpl/test/rwvmtests"}, &vmtest.Options{
+		QEMUOpts: qemu.Options{
+			Devices: []qemu.Device{
+				vmdriver.HostNetwork{
+					Net: &net.IPNet{
+						// 192.168.0.0/24
+						IP:   net.IP{192, 168, 0, 0},
+						Mask: net.CIDRMask(24, 32),
+					},
+				},
+			},
+			KernelArgs: fmt.Sprintf("P9_PORT=%d P9_TARGET=192.168.0.2", serverPort),
+			Timeout:    30 * time.Second,
+		},
+	})
+}
+
+func TestBenchmark(t *testing.T) {
+	// Needs to definitely be in a tmpfs for performance testing.
+	tempDir, err := ioutil.TempDir("/dev/shm", "localfs-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	serverSocket, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatalf("err binding: %v", err)
+	}
+	defer serverSocket.Close()
+	serverPort := serverSocket.Addr().(*net.TCPAddr).Port
+
+	// Run the server. No logger -- slows down the benchmark.
+	s := p9.NewServer(Attacher(tempDir)) //, p9.WithServerLogger(ulogtest.Logger{TB: t}))
+	go s.Serve(serverSocket)
+
+	// Run the read-write tests from fsimpl/test/rwvm.
+	vmtest.GolangTest(t, []string{"github.com/hugelgupf/p9/fsimpl/test/benchmark"}, &vmtest.Options{
+		BuildOpts: uroot.Opts{
+			ExtraFiles: []string{
+				"/usr/bin/dd:bin/dd",
+			},
+		},
 		QEMUOpts: qemu.Options{
 			Devices: []qemu.Device{
 				vmdriver.HostNetwork{
