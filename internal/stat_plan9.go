6267407f503f00bd7dce760a9543a0b6a2a8749a
Ronald G Minnich <rminnich@gmail.com>
2025-06-03 07:42:33 -0700

    Add plan 9 server support
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

---
+++ internal/stat_plan9.go
@@ -0,0 +1,47 @@
+package internal
+
+import (
+	"os"
+)
+
+// NOTE: taken from amd64 Linux
+type Timespec struct {
+        Sec  int64
+        Nsec int64
+}
+
+type Stat_t struct {
+	Dev     uint64
+	Ino     uint64
+	Nlink   uint64
+	Mode    uint32
+	Uid     uint32
+	Gid     uint32
+	Rdev    uint64
+	Size    int64
+	Blksize int64
+	Blocks  int64
+	Atim    Timespec
+	Mtim    Timespec
+	Ctim    Timespec
+}
+
+// InfoToStat takes a platform native FileInfo and converts it into a 9P2000.L compatible Stat_t
+func InfoToStat(fi os.FileInfo) *Stat_t {
+	t := Timespec{Sec: int64(fi.ModTime().Second())}
+	return &Stat_t{
+		Dev:     0,
+		Ino:     0,
+		Nlink:   1,
+		Mode:    uint32(fi.Mode()),
+		Uid:     0,
+		Gid:     0,
+		Rdev:    0,
+		Size:    fi.Size(),
+		Blksize: 8192,
+		Blocks:  fi.Size() / 8192,
+		Atim:    t,
+		Mtim:    t,
+		Ctim:    t,
+	}
+}
--- linux/errors_unix.go
+++ linux/errors_unix.go
@@ -1,5 +1,5 @@
-//go:build !windows && !linux
-// +build !windows,!linux
+//go:build !windows && !linux && !plan9
+// +build !windows,!linux,!plan9
 
 package linux
 
