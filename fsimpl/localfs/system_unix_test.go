be262b60fcb30d723360cbac8fde01cdc3409748
Chris Koch <c@chrisko.ch>
2026-05-15 15:25:48 -0700

    Fix encodeLikely test for Mac
    Signed-off-by: Chris Koch <c@chrisko.ch>

--- fsimpl/localfs/system_unix_test.go
+++ fsimpl/localfs/system_unix_test.go
@@ -1,10 +1,20 @@
+//go:build unix
+
 package localfs
 
 import (
+	"runtime"
 	"testing"
+
+	"golang.org/x/sys/unix"
 )
 
 func TestEncodeLikely(t *testing.T) {
+	var maxMajorMinorBits = 12
+	if runtime.GOOS == "darwin" {
+		maxMajorMinorBits = 8
+	}
+
 	tests := []struct {
 		name       string
 		dev        uint64
@@ -14,14 +24,14 @@ func TestEncodeLikely(t *testing.T) {
 	}{
 		{
 			name:       "Simple Valid Case",
-			dev:        mkDev(0, 0),
+			dev:        unix.Mkdev(0, 0),
 			ino:        123,
 			wantOk:     true,
 			wantResult: 123 | (0 << 39) | (0 << 51),
 		},
 		{
 			name:       "Various different numbers",
-			dev:        mkDev(123, 456),
+			dev:        unix.Mkdev(123, 456),
 			ino:        789,
 			wantOk:     true,
 			wantResult: 789 | (456 << 39) | (123 << 51),
@@ -35,10 +45,10 @@ func TestEncodeLikely(t *testing.T) {
 		},
 		{
 			name:       "Max Likely Major Minor",
-			dev:        mkDev(1<<12-1, 1<<12-1),
+			dev:        unix.Mkdev(1<<maxMajorMinorBits-1, 1<<maxMajorMinorBits-1),
 			ino:        123,
 			wantOk:     true,
-			wantResult: 123 | (1<<12-1)<<39 | (1<<12-1)<<51,
+			wantResult: 123 | (1<<maxMajorMinorBits-1)<<39 | (1<<maxMajorMinorBits-1)<<51,
 		},
 		{
 			name:   "Inode Too Large",
@@ -48,13 +58,13 @@ func TestEncodeLikely(t *testing.T) {
 		},
 		{
 			name:   "Minor Too Large",
-			dev:    mkDev(0, (1 << 12)),
+			dev:    unix.Mkdev(0, (1 << 12)),
 			ino:    1,
 			wantOk: false,
 		},
 		{
 			name:   "Major Too Large",
-			dev:    mkDev((1 << 12), 0),
+			dev:    unix.Mkdev((1 << 12), 0),
 			ino:    1,
 			wantOk: false,
 		},
@@ -78,11 +88,4 @@ func TestEncodeLikely(t *testing.T) {
 			}
 		})
 	}
-}
-
-func mkDev(major, minor uint32) uint64 {
-	return (uint64(major) & 0x00000fff << 8) |
-		(uint64(major) & 0xfffff000 << 32) |
-		(uint64(minor) & 0x000000ff << 0) |
-		(uint64(minor) & 0xffffff00 << 12)
 }
